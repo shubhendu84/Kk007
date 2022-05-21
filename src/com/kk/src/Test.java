2415dae0d88750bec765f0b71f8c4645f20bb014
shubhendu kumar <tech.shubhendu@gmail.com>
2022-05-21 17:03:34 +0530

    initial commit

---
+++ src/com/kk/src/Test.java
@@ -0,0 +1,19 @@
+package com.kk.src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		// adding data into arraylist
+
+		List list = new ArrayList<String>();
+		list.add("Ramu");
+		list.add("shyam");
+		list.add("Bharat");
+		list.add("satrughan");
+
+	}
+}
